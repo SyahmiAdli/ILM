cef94929d6435ca24d06f165ae58c07d8c7b3c30
SyahmiAdli <100678059+SyahmiAdli@users.noreply.github.com>
2022-03-02 16:22:23 +0800

    Initial commit

---
+++ Syah.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+
+st.sidebar.header('User Input Values')
+
+
+
+
---
+++ myfirstapp.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+st.header("Know Your Performance in Examination")
+
+
